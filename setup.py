d354a49fde258f644c6f80fdc8ce227a43d6f5a9
Your Name <you@example.com>
2021-03-23 15:33:03 +0530

    added setup.py file for creating own lib and tox

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup,find_packages
+
+setup(
+    name = "src",
+    version = "0.0.1",description = "it is a wine quality package",
+    author = "ravi441",
+    packages = find_packages(),
+    license = "MIT"
+)
+#
+# you_numpy
+#    you_numpy
+#    __init__
